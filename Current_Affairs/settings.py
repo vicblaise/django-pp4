5f8606d7a1075fbf494c1e0e5fcd273d911a4361
Victor Ogwuche <vblaise967@gmail.com>
2025-02-07 16:37:32 +0000

    install database and created superuser

--- Current_Affairs/settings.py
+++ Current_Affairs/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3-ow%zm5f#30!f#&8)9m)g!cu_1@mz!+@+ve&j=1@%x$ob_6(!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-vicblaise-djangopp4-ug2l0e192g1.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -74,11 +78,16 @@ WSGI_APPLICATION = 'Current_Affairs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+#DATABASES = {
+#   'default': {
+#      'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
+#}
+#}
+
+DATABASES ={
+    'default':
+        dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
