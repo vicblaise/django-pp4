b157fd914630adb238feb0c24f7569a228ba8343
Victor Ogwuche <vblaise967@gmail.com>
2025-02-08 22:30:47 +0000

    Add secret key

--- Current_Affairs/settings.py
+++ Current_Affairs/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3-ow%zm5f#30!f#&8)9m)g!cu_1@mz!+@+ve&j=1@%x$ob_6(!'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
