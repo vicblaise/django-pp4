89d6400cac16eb788242c90e4cc33e4fcfa983e4
Victor Ogwuche <vblaise967@gmail.com>
2025-02-09 19:51:23 +0000

    Add generic post view

--- Current_Affairs/settings.py
+++ Current_Affairs/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-vicblaise-djangopp4-ug2l0e192g1.ws.codeinstitute-ide.net','.herokuapp.com']
 
--- Current_Affairs/urls.py
+++ Current_Affairs/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from news.views import daily_news
+from django.urls import path, include
+#from news.views import daily_news#
 
 
 urlpatterns = [
+    path("", include("news.urls"), name="news-urls"),
     path('admin/', admin.site.urls),
-    path('news/', daily_news, name='news'),
 ]
