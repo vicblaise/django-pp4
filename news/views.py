0f20e304225c13368a3aca7fc0292700be4c6d70
Victor Ogwuche <vblaise967@gmail.com>
2025-02-09 21:07:32 +0000

    Add css styling

--- news/views.py
+++ news/views.py
@@ -9,4 +9,5 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.all()
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
